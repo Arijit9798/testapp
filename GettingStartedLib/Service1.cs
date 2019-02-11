9c7a39a719fc4f778ad1ba28fe6850c8b3e932c8
Arijit9798 <47169195+Arijit9798@users.noreply.github.com>
2019-02-11 09:25:01 +0530

    Add files via upload

---
+++ GettingStartedLib/Service1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingStartedLib
+{
+    public class CalculatorService : ICalculator
+    {
+        //public double Add(double n1, double n2)
+        //{
+        //    double result = n1 + n2;
+        //    Console.WriteLine("Received Add({0},{1})", n1, n2);
+        //    // Code added to write output to the console window.
+        //    Console.WriteLine("Return: {0}", result);
+        //    return result;
+        //}
+
+        //public double Subtract(double n1, double n2)
+        //{
+        //    double result = n1 - n2;
+        //    Console.WriteLine("Received Subtract({0},{1})", n1, n2);
+        //    Console.WriteLine("Return: {0}", result);
+        //    return result;
+        //}
+
+        //public double Multiply(double n1, double n2)
+        //{
+        //    double result = n1 * n2;
+        //    Console.WriteLine("Received Multiply({0},{1})", n1, n2);
+        //    Console.WriteLine("Return: {0}", result);
+        //    return result;
+        //}
+
+        //public double Divide(double n1, double n2)
+        //{
+        //    double result = n1 / n2;
+        //    Console.WriteLine("Received Divide({0},{1})", n1, n2);
+        //    Console.WriteLine("Return: {0}", result);
+        //    return result;
+        //}
+        
+        public async Task<string> SomeTask()
+        {
+            string result = string.Empty;
+            using (var client = new HttpClient())
+            {
+               // client.BaseAddress = new Uri("http://localhost:7071/api/Function1");
+                var reponse = await client.GetAsync("http://localhost:7071/api/Function1?name=sri");
+                result = await reponse.Content.ReadAsStringAsync();
+                
+            }
+            return result;
+        }
+
+    }
+
+}
